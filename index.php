33febe73e8a17f2740ec97b0ff16f58b4a4c396b
TobiasK25 <tobika2000.tk@gmail.com>
2017-07-17 20:37:34 +0200

    footer added

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ session_start();
     <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0">
-<link type="text/css" rel="stylesheet" href="css/maintenance.css">
-<link type="text/css" rel="stylesheet" href="css/reset.css">
+    <link type="text/css" rel="stylesheet" href="css/maintenance.css">
+    <link type="text/css" rel="stylesheet" href="css/reset.css">
 </head>
 
 <body>
-        <div class="container">
-            <div class="subcontainer">
-                <?php
+    <div class="container">
+        <div class="subcontainer">
+            <?php
                 if (isset($_GET['site'])) {
                     $site = ucfirst($_GET['site']);
                     echo '<p>' . $site . " is temporarily down for maintenance. We should be back online shortly!</p>";
@@ -24,7 +24,8 @@ session_start();
                     echo '<p>Temporarily down for maintenance! We should be back online shortly!';
                 }
                 ?>
-                <p><br><br> <?php
+                <p><br><br>
+                    <?php
                     $a = array(
                         "Stay calm and drink something.",
                         "Grab yourself a bag of chips!",
@@ -33,9 +34,21 @@ session_start();
                     $random_keys = array_rand($a);
                     echo $a[$random_keys];
                     ?>
-                    </p><br><br><p class="contact">Contact: <a href="mailto:support@intranetproject.net">support@intranetproject.net</a></p>
+                </p><br><br>
+                <p class="contact">Contact: <a href="mailto:support@intranetproject.net">support@intranetproject.net</a></p>
+        </div>
+        <div class="footer">
+            <div class="git">
+                <p><a href="https://github.com/IntranetProject">GitHub</a></p>
+            </div>
+            <div class="imp">
+                <p><a href="imprint.html">Impressum</a></p>
+            </div>
+            <div class="credit">
+                <p><b><i style="color: black;">&lt; &gt;</i></b> with <i style="color: red;">&hearts;</i> by <a href="https://github.com/IntranetProject">Intranet</a></p>
             </div>
         </div>
+    </div>
 </body>
 
 </html>
