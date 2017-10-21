8e28d77b92821e55f36a6ed5c5024ff3b4396254
Tobi2K <tobika2000.tk@gmail.com>
2017-10-21 14:55:37 +0200

    revision of style

--- index.php
+++ index.php
@@ -8,47 +8,66 @@ session_start();
 <head>
     <title></title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="css/style.css">
-    <link type="text/css" rel="stylesheet" href="css/reset.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </head>
 
 <body>
+  <main>
     <div class="container">
-        <div class="subcontainer">
-            <?php
+        <div class="center-align page-panel header">
+            <h4><?php
                 if (isset($_GET['site'])) {
                     $site = ucfirst($_GET['site']);
-                    echo '<p>' . $site . " is temporarily down for maintenance. We should be back online shortly!</p>";
+                    echo '<p>' . $site . " is temporarily down for maintenance. We should be back online shortly!</h4>";
                 } else {
-                    echo '<p>Temporarily down for maintenance! We should be back online shortly!';
+                    echo '<h4>Temporarily down for maintenance! We should be back online shortly!';
                 }
-                ?>
-                <p><br>
-                    <?php
-                    $a = array(
-                        "Stay calm and drink something.",
-                        "Grab yourself a bag of chips!",
-                        "Donate us some money.");
-
-                    $random_keys = array_rand($a);
-                    echo $a[$random_keys];
-                    ?>
-                </p><br>
+                ?></h4>
                 <p class="contact">Contact: <a href="mailto:support@intranetproject.net">support@intranetproject.net</a></p>
         </div>
-        <div class="footer">
-            <div class="git">
-                <p><a href="https://github.com/IntranetProject">GitHub</a></p>
-            </div>
-            <div class="imp">
-                <p><a href="imprint.html">Impressum</a></p>
-            </div>
-            <div class="credit">
-                <p><b><i style="color: white;">&lt; &gt;</i></b> with <i style="color: red;">&hearts;</i> by <a href="https://github.com/IntranetProject">Intranet</a></p>
-            </div>
+      </main>
+      <footer class="page-footer teal lighten-2">
+        <div class="container">
+           <div class="row">
+              <div class="col l6 s12">
+                 <h5 class="white-text">How about you...</h5>
+                 <p class="grey-text text-lighten-4">
+                     <?php
+                     $a = array(
+                         "... stay calm and drink something.",
+                         "... grab yourself a bag of chips!",
+                         "... donate us some money.",
+                         "... try again later.",
+                         "... visit our main site <a href='http://intranetproject.net' style='color:white; text-decoration: underline;'>here</a>");
+
+                     $random_keys = array_rand($a);
+                     echo $a[$random_keys];
+                     ?>
+                 </p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                 <h5 class="white-text">Links</h5>
+                 <ul>
+                    <li><a class="grey-text text-lighten-3" href="https://github.com/IntranetProjec">GitHub</a></li>
+                    <li><a class="grey-text text-lighten-3" href="imprint.html">Imprint</a></li>
+                    <li>
+                       <p class="grey-text text-lighten-3" href="#!">&lt; &gt;</i></b> with <i style="color: red;">&hearts;</i> by <a class="grey-text text-lighten-3" href="https://github.com/IntranetProject">Intranet</a></p>
+                    </li>
+                 </ul>
+              </div>
+           </div>
         </div>
-    </div>
+        <div class="footer-copyright">
+           <div class="container">
+              © 2017 Intranet Project
+           </div>
+        </div>
+      </footer>
 </body>
 
 </html>
