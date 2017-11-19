9d36458d7d3b011bb4d11f5905249d5752f9c1be
Bennet Gallein <bennet@intranetproject.net>
2017-11-19 18:48:31 +0100

    added something cool

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ session_start();
   <main>
     <div class="container">
         <div class="center-align page-panel header">
+
             <h4><?php
                 if (isset($_GET['site'])) {
                     $site = ucfirst($_GET['site']);
@@ -27,8 +28,9 @@ session_start();
                 } else {
                     echo '<h4>Temporarily down for maintenance! We should be back online shortly!';
                 }
-                ?></h4>
+                ?></h4>                
                 <p class="contact">Contact: <a href="mailto:support@intranetproject.net">support@intranetproject.net</a></p>
+                <img src="https://i.imgur.com/WesZdDe.gif"></img>
         </div>
       </main>
       <footer class="page-footer teal lighten-2">
