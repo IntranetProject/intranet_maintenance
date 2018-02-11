925b14229b80f67e22d5bd0e9c7ca12616eb40e5
Tobi2K <tobika2000.tk@gmail.com>
2018-02-11 19:11:35 +0100

    color changes

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ session_start();
             </div>
         </div>
     </main>
-    <footer class="page-footer teal lighten-2">
+    <footer class="page-footer blue">
         <div class="container">
             <div class="row">
                 <div class="col l6 s12">
